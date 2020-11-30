c4f92ee203cf901f9f7dc433bccc8007b8eaccef
github username <github email address>
2020-11-29 21:07:26 -0800

    added icons and a tags

--- src/Components/NewsCard.js
+++ src/Components/NewsCard.js
@@ -3,6 +3,7 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import BookmarkIcon from '@material-ui/icons/Bookmark';
 import BookmarkBorderOutlinedIcon from '@material-ui/icons/BookmarkBorderOutlined';
+import OpenInNewOutlinedIcon from '@material-ui/icons/OpenInNewOutlined';
 import Grid from '@material-ui/core/Grid'
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -34,7 +35,9 @@ export default function NewsCard({ newsItem, clickHandler, search }) {
             return (
                 <CardContent>
                     {clicked ? <BookmarkIcon>article_saved</BookmarkIcon> : <BookmarkBorderOutlinedIcon className={classes.favorite} onClick={addToFavorites}>save_article</BookmarkBorderOutlinedIcon>}
-                    <h3><strong>{newsItem.title}</strong></h3>
+                    <a href={newsItem.url} target="_blank">
+                        <h3><strong>{newsItem.title}</strong><OpenInNewOutlinedIcon >open_article</OpenInNewOutlinedIcon></h3>
+                    </a>
                     <p>{newsItem.abstract}</p>
                 </CardContent>
             );
