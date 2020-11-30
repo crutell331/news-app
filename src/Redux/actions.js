9f706c28ff2c80466cdb36a683c1505d92446a93
github username <github email address>
2020-11-30 10:02:32 -0800

    hide API keys since going to fork into a public repo

--- src/Redux/actions.js
+++ src/Redux/actions.js
@@ -2,7 +2,7 @@ import types from './actionTypes';
 
 export function fetchTrending() {
     return async function (dispatch) {
-        const response = await fetch('https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=bFROkYUGoTuFFJJlgnugF4JeYORGSKgY')
+        const response = await fetch(`https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=${process.env.REACT_APP_NYT_KEY}`)
         const data = await response.json();
         dispatch({ type: types.fetchTrending, payload: data.results });
     };
